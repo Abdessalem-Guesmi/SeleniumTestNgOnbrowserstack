f26f858c6f5b05120a84153b5c09d8fd927f4088
Abdessalem Guesmi <guesmiabdessalem@yahoo.fr>
2021-02-05 22:27:11 +0100

    updated - v4

--- SeleniumTestNgOnBrowserStack/src/test/java/base/BaseTests.java
+++ SeleniumTestNgOnBrowserStack/src/test/java/base/BaseTests.java
@@ -1,26 +1,15 @@
 package base;
 
-import java.io.File;
-import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-import com.google.common.io.Files;
 
 import pages.GooglePage;
 
@@ -28,52 +17,23 @@ public class BaseTests {
 
 	public WebDriver driver;
 
-	private static ExtentHtmlReporter htmlReport;
-	private static ExtentReports extent;
 	protected static GooglePage googlePage;
 	public static final String USERNAME = "abdessalemguesmi1";
 	public static final String AUTOMATE_KEY = "aWqz2o3HymSuBheDSWs9";
 	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
-	public static ExtentReports getInstance() {
-
-		String reportName = getReporttName();
-		String directory = System.getProperty("user.dir") + "/reports/";
-		new File(directory).mkdirs();
-		String path = directory + reportName;
-		htmlReport = new ExtentHtmlReporter(path);
-		htmlReport.config().setEncoding("utf-8");
-		htmlReport.config().setDocumentTitle("Automation Test");
-		htmlReport.config().setTheme(Theme.STANDARD);
-
-		extent = new ExtentReports();
-		extent.setSystemInfo("organization", "abdou guesmi");
-		extent.setSystemInfo("Browser", "Chrome");
-		extent.setSystemInfo("organization", "abdou guesmi");
-		extent.attachReporter(htmlReport);
-
-		// WebDriverManager.chromedriver().setup();
-		return extent;
-	}
-
-	public static String getReporttName() {
-		Date d = new Date();
-		String fileName = "Report_" + d.toString().replace(":", "-") + ".html";
-		return fileName;
-
-	}
-
-	@BeforeMethod(alwaysRun = true)
-	public void setUp(ITestResult iTestResult) throws MalformedURLException {
+	@BeforeMethod
+	public void setUp(Method method) throws MalformedURLException {
+		System.out.println(URL);
 		DesiredCapabilities caps = new DesiredCapabilities();
 
 		caps.setCapability("os", "Windows");
 		caps.setCapability("os_version", "10");
 		caps.setCapability("browser", "Chrome");
 		caps.setCapability("browser_version", "latest");
-		caps.setCapability("browserstack.idleTimeout", 180);
+		// caps.setCapability("browserstack.idleTimeout", 180);
 		caps.setCapability("name", "Abdessalem");
-		caps.setCapability("name", "Google_" + iTestResult.getMethod().getMethodName());
+		caps.setCapability("name", "Google_" + method.getName());
 		driver = new RemoteWebDriver(new URL(URL), caps);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -83,21 +43,9 @@ public class BaseTests {
 	}
 
 	@AfterMethod
-	public void recordFailure(ITestResult result) {
-		if (ITestResult.FAILURE == result.getStatus()) {
-			TakesScreenshot camera = (TakesScreenshot) driver;
-			File screenshot = camera.getScreenshotAs(OutputType.FILE);
-			try {
-				Files.move(screenshot, new File("screenshot/" + result.getName() + ".png"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		driver.close();
-	}
-
-	@AfterClass
 	public void tearDown() {
+
 		driver.quit();
 	}
+
 }
--- SeleniumTestNgOnBrowserStack/src/test/java/test/Test1.java
+++ SeleniumTestNgOnBrowserStack/src/test/java/test/Test1.java
@@ -13,33 +13,33 @@ public class Test1 extends BaseTests {
 
 	@Test(priority = 1)
 	public void test1() {
-		title = sendKeyWord("Test1");
+		title = getTitle("Test1");
 		Assert.assertTrue(title.contains("Test0"), "the title contains not Test0");
 	}
 
 	@Test(priority = 2)
 	public void test2() {
-		title = sendKeyWord("Test2");
+		title = getTitle("Test2");
 		Assert.assertTrue(title.contains("Test2"), "the title contains not Test4");
 
 	}
 
 	@Test(priority = 3)
 	public void test3() {
-		sendKeyWord("Test3");
+		getTitle("Test3");
 		throw new SkipException("test-case skipped");
 
 	}
 
 	@Test(priority = 4)
 	public void test4() {
-		title = sendKeyWord("Test4");
+		title = getTitle("Test4");
 
 		Assert.assertTrue(title.contains("Test4"), "the title contains not Test4");
 
 	}
 
-	public static String sendKeyWord(String keyword) {
+	public static String getTitle(String keyword) {
 		googlePage.sendKeyWord(keyword);
 		homePage = googlePage.clickSearchBtn();
 		return homePage.getTitle();
